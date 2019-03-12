4e60de959f56ddfa736a6f972b2cd1d7a010093a
majinmaru <gotch11@naver.com>
2019-03-12 18:25:41 +0900

    190312
    효과시작

---
+++ javascripts/rent.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$(".color-tab").click(function(e){
+		if($(this).hasClass('current')){
+			$(this).removeClass('current');
+		} else {
+			$(this).addClass('current');
+		}
+		e.preventDefault();
+	});
+
+	$(".category-tab").click(function(e){
+		$(this).addClass('current').siblings().removeClass('current');
+		e.preventDefault();
+	});
+
+	$(".quickmenu-tab.add").click(function(e){
+		$(".shopbag").addClass('active');
+		setTimeout(function(){
+			$(".shopbag").removeClass('active');
+		}, 2000);
+		e.preventDefault();
+	});
+});
